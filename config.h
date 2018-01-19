4a09e2fd6c9669c3961feb2ccc8902f6ea479c14
Matthias Endler <matthias-endler@gmx.net>
2018-01-19 13:36:57 +0100

    Upload hacked version

---
+++ config.h
@@ -0,0 +1,92 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Whether to build opcache as dynamic module */
+#define COMPILE_DL_OPCACHE 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Struct flock is BSD-type */
+/* #undef HAVE_FLOCK_BSD */
+
+/* Struct flock is Linux-type */
+#define HAVE_FLOCK_LINUX /**/
+
+/* Define to enable copying PHP CODE pages into HUGE PAGES (experimental) */
+#define HAVE_HUGE_CODE_PAGES 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define if you have mprotect() function */
+#define HAVE_MPROTECT 1
+
+/* Define to enable file based caching (experimental) */
+#define HAVE_OPCACHE_FILE_CACHE 1
+
+/* Define if you have SysV IPC SHM support */
+#define HAVE_SHM_IPC 1
+
+/* Define if you have mmap(MAP_ANON) SHM support */
+#define HAVE_SHM_MMAP_ANON 1
+
+/* Define if you have mmap() SHM support */
+#define HAVE_SHM_MMAP_FILE 1
+
+/* Define if you have POSIX mmap() SHM support */
+/* #undef HAVE_SHM_MMAP_POSIX */
+
+/* Define if you have mmap("/dev/zero") SHM support */
+#define HAVE_SHM_MMAP_ZERO 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <sys/uio.h> header file. */
+#define HAVE_SYS_UIO_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME ""
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING ""
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME ""
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION ""
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
